475d107a5df78e12db4f9eb77b322f3f14c8d62a
Jun young <980218qwe@naver.com>
2022-05-26 05:02:14 +0900

    내 동아리는 아직 미완성

---
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.capstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton search;
+    ImageButton activity;
+    ImageButton visit;
+    ImageButton option;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
