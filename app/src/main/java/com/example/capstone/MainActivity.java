c3dbc4329ce3e64ecb98464efcf96fdff9833d99
Jun young <980218qwe@naver.com>
2022-05-30 13:48:37 +0900

    update

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
@@ -14,12 +14,16 @@ public class MainActivity extends AppCompatActivity {
     ImageButton visit;
     ImageButton option;
 
+    TextView activity_button;
+    TextView option_button;
+    TextView visit_button;
+    TextView search_box''
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
 
